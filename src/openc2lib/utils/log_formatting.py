b93ddd31aedd0d3d81dbcc7d0aaca2848479452c
Matteo Repetto <matteo.repetto@cnit.it>
2024-07-31 10:49:12 +0200

    Added timestamping option to logFormatter

--- src/openc2lib/utils/log_formatting.py
+++ src/openc2lib/utils/log_formatting.py
@@ -11,6 +11,7 @@
 """
 
 import logging
+import datetime
 
 class LogFormatter(logging.Formatter):
 	""" Colored logging formatter
@@ -29,12 +30,14 @@ class LogFormatter(logging.Formatter):
 	highlight_red = '\x1b[37;1m\x1b[41m'
 	reset = '\x1b[0m'
 
-	def __init__(self, datetime= True, name=True):
+	def __init__(self, datetime= True, name=True, datefmt=None):
 		""" Set the custom format
 
 			  Select which optional fields will be included.
 			  :param datetime: Set to `False` to disable date/time indication.
 			  :param name: Set to `False` to diable module name indication.
+			  :param datefmt: Format the date according to this string. Takes common
+			  		time.strftime specifiers, plus the '%t' which gives the timestamp.
 		"""
 		super().__init__()
 		if datetime:
@@ -52,10 +55,26 @@ class LogFormatter(logging.Formatter):
 		    logging.ERROR: self.red,
 		    logging.CRITICAL: self.highlight_red
 		}
+		self.datefmt = datefmt
 
 	def format(self, record):
 		""" Color the record according to the log level """
 		log_fmt = self.fmt.replace('COLOR', self.COLORS[record.levelno])
-		formatter = logging.Formatter(log_fmt, style='{')
+		self.fmt = self.fmt
+		if self.datefmt:
+			datefmt = self.datefmt.replace('%t', "{:.6f}".format((datetime.datetime.fromtimestamp(record.created)).timestamp()))
+		else:
+			datefmt = None
+		# We are indeed inheriting from logging.Formatter, but looks like
+		# there is no way to change the format string at runtime.
+		formatter = logging.Formatter(log_fmt, datefmt, style='{')
 		return formatter.format(record)
+
+	def formatTime(record, datefmt=None):
+		""" This function is invoked when (asctime) is used in the format string """
+		if datefmt:
+			datefmt.replace('%t', str(record.created))
+		super().formatTime(record, datefmt)
+
+
 
