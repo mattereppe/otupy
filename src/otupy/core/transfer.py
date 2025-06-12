aaf554627b84f31af5d0f9cde9c0982fae2e3b9e
Matteo Repetto <matteo.repetto@cnit.it>
2025-06-12 08:56:30 +0200

    Added more detail to Transfer documentation

--- src/otupy/core/transfer.py
+++ src/otupy/core/transfer.py
@@ -14,9 +14,18 @@ class Transfer:
 
 			Encodes, sends a message, and returns the response.
 
+			Implementations of this method may return either a plain `Message` or a list of `Messages`; the latter
+			option is expected for those implementation that support delivery to multiple Consumers. Each `Transfer`
+			implmentation shall use either options consistently (e.g., it should always return either a plain object or
+			a list of objects).
+			Callers of this method must be prepared to manage the two cases, by leveraging the knowledge of the `Transfer`
+			implementation they are using. 
+			In any case, implementations must guarantee the uniqueness of returned Messages (i.e., duplicates must
+			be discarded by the Transfer protocol). 
+
 			:arg msg: an otupy `Message` to send
 			:arg encoder: the `Encoder` to be used 
-			:return: An otupy `Message` that contains the `Response` to the sent Message.
+			:return: An otupy `Message` or a list of `Message`s that contains the `Response`(s) to the sent `Command`.
 		"""
 		pass
 
@@ -25,6 +34,9 @@ class Transfer:
 			
 			Listen for incoming `Message`s and dispatches them to the `Actuator`. This method may
 			be blocking or non-blocking. 
+			
+			The expected signature for the `callback` function:
+				`callback(cmd: otupy.Message)`
 
 			:arg callback: the `Consumer.dispatch` function that contains the logic to dispatch a `Message`
 				to one or more `Actuator`
