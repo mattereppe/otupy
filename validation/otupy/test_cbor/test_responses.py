55c13a7623b8d37274032cafb77b73c7faa1916d
Matteo Repetto <matteo.repetto@cnit.it>
2025-06-13 22:12:14 +0200

    updated cbor validation tests

--- validation/otupy/test_cbor/test_responses.py
+++ validation/otupy/test_cbor/test_responses.py
@@ -1,14 +1,14 @@
 import pytest
 import os
 import ipaddress
-import yaml
+import cbor2
 
-from helpers import load_yaml, load_files
+from helpers import load_cbor, load_files
 from otupy import Encoder, Response, StatusCode, EncoderError
 import otupy.profiles.slpf
 import otupy.transfers.http
 import otupy.transfers.http.message as http
-from otupy.encoders.yaml import YAMLEncoder
+from otupy.encoders.cbor import CBOREncoder
 
 import sys
 sys.path.insert(0, "../profiles/")
@@ -22,9 +22,9 @@ import digits
 import digits_and_chars
 
 
-# Parameters to get good and bad samples of yaml messages
-response_path_good = "../../openc2-yaml-samples/tests/responses/good"
-response_path_bad = "../../openc2-yaml-samples/tests/responses/bad"
+# Parameters to get good and bad samples of cbor messages
+response_path_good = "../../openc2-cbor-samples/tests/responses/good"
+response_path_bad = "../../openc2-cbor-samples/tests/responses/bad"
 
 # WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING 
 # 
@@ -33,40 +33,40 @@ response_path_bad = "../../openc2-yaml-samples/tests/responses/bad"
 # I removed the trailing "-draft-2019-02" e put the original file in the bad examples.
 
 
-@pytest.mark.parametrize("rsp", load_yaml(response_path_good) )
+@pytest.mark.parametrize("rsp", load_cbor(response_path_good) )
 @pytest.mark.dependency(name="test_decoding")
 def test_decoding(rsp):
-	print("Response yaml: ", rsp)
+	print("Response cbor: ", rsp)
 	c = Encoder.decode(Response, rsp)
 	assert type(c) == Response
 
-@pytest.mark.parametrize("rsp", load_yaml(response_path_good) )
+@pytest.mark.parametrize("rsp", load_cbor(response_path_good) )
 #@pytest.mark.dependency(name="test_encoding", depends=["test_decoding"])
 def test_encoding(rsp):
-	print("Response yaml: ", rsp)
+	print("Response cbor: ", rsp)
 	oc2_rsp = Encoder.decode(Response, rsp)
 	# Use to dict because the Encoder.encode method returns a str
-	oc2_yaml = Encoder.todict(oc2_rsp)
-	print(oc2_yaml)
+	oc2_cbor = Encoder.todict(oc2_rsp)
+	print(oc2_cbor)
 
 #	fix_ip_addresses(cmd)
 #	fix_hex(cmd)
 #	fix_uuid(cmd)
-	assert rsp == oc2_yaml
+	assert rsp == oc2_cbor
 
 @pytest.mark.parametrize("rsp_file", load_files(response_path_bad) )
 def test_decoding_invalid(rsp_file):
 	""" Check invalid commands raise exceptions when decoded """
 	print("Response file: ", rsp_file)
 	# It may also raises while loading the files, since they may be empty
-	with open(rsp_file, 'r') as frsp:
+	with open(rsp_file, 'rb') as frsp:
 		# There is one empty file that raises this exception
 		try:
-			rsp = yaml.safe_load(frsp) 
-		except yaml.decoder.JSONDecodeError:
+			rsp = cbor2.load(frsp) 
+		except cbor2.CBORDecodeError:
 			rsp = ""
 
-		print("Response yaml: ", rsp)
+		print("Response cbor: ", rsp)
 		with pytest.raises( (ValueError, KeyError, EncoderError) ):
 			Encoder.decode(Response, rsp)
 		
