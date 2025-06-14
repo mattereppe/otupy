c31a1c3e2138a5ddce2afad1804ff70b282cb836
Matteo Repetto <matteo.repetto@cnit.it>
2025-06-15 01:31:47 +0200

    Added mqtt validation

--- validation/otupy/conftest.py
+++ validation/otupy/conftest.py
@@ -5,6 +5,7 @@ import datetime
 from otupy import *
 import otupy.profiles.slpf as slpf
 from otupy.transfers.http import HTTPTransfer
+from otupy.transfers.mqtt import MQTTTransfer, OpenC2Role
 from otupy.encoders.json import  JSONEncoder
 from otupy.encoders.yaml import  YAMLEncoder
 from otupy.encoders.xml import  XMLEncoder
@@ -37,7 +38,7 @@ def create_producer():
 
 @pytest.fixture
 def create_producer_mqtt():
-	producer = Producer("OpenC2_Producer",
+	producer = Producer("myproducer",
 	                     JSONEncoder(),
                         MQTTTransfer("150.145.8.217", 1883, 
 									OpenC2Role.Producer, device_id="myproducer"))
@@ -79,6 +80,11 @@ def http_headers():
 @pytest.fixture
 def http_body():
 	return  {'headers': { "request_id": "0e3d8fa8-0bae-4055-a341-9c97b4f328f7", "created": 1545257700000, "from": "test-producer", "to": [ "test-consumer" ] },
+		"body": { "openc2": { "request": {} }}}
+
+@pytest.fixture
+def mqtt_body():
+	return  {'headers': { "request_id": "0e3d8fa8-0bae-4055-a341-9c97b4f328f7", "created": 1545257700000, "from": "myproducer", "to": [ "testconsumer" ] },
 		"body": { "openc2": { "request": {} }}}
 
 
