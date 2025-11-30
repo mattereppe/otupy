a1f97462edc79c0b1cf2bc840e43185e9e172af1
Daniele Canavese <daniele.canavese@gmail.com>
2025-11-30 14:59:49 +0100

    The configuration files for the connector are now YAML files

--- src/otupy/apps/connector/connector.py
+++ src/otupy/apps/connector/connector.py
@@ -1,8 +1,10 @@
 """The connector."""
+
 from argparse import ArgumentParser
-from configparser import ConfigParser
 from glob import glob
 from os.path import dirname
+
+from yaml import safe_load
 
 # noinspection PyUnusedImports
 import otupy.actuators  # Do not remove! It is necessary to find the registered actuators.
@@ -22,78 +24,50 @@ def main() -> None:
     """
     # Parse the CLI arguments.
     arguments = ArgumentParser()
-    arguments.add_argument("-c", "--config", default=f"{dirname(__file__)}/connector.ini",
+    arguments.add_argument("-c", "--config", default=f"{dirname(__file__)}/connector.yaml",
                            help="path to the configuration file")
     args = arguments.parse_args()
 
     # Parse the configuration file.
-    config = ConfigParser()
-    config.read(args.config)
-    ip = config["connector"].get("ip")
-    port = config["connector"].getint("port")
-    endpoint = config["connector"].get("endpoint")
-    protocol = config["connector"].getint("protocol")
-    transfer = config["connector"].get("transfer")
-    encoding = config["connector"].get("encoding")
-    hostname = config["connector"].get("hostname")
-    configs = config["connector"].get("configs")
+    with open(args.config) as config_file:
+        config = safe_load(config_file)
 
-    actuators = {}
-    for file in glob(f"{configs}/**/*.ini", recursive=True):
-        actuator_config = ConfigParser()
-        actuator_config.read(file)
-        for name in actuator_config.sections():
-            print(f"Loading {name}...")
-            identifier = actuator_config[name].get("id")
-            if identifier not in Actuators:
-                raise RuntimeError(f"{identifier} is not a registered actuator")
-            clazz = Actuators[identifier]
-            parameters = {
-                "asset_id": name,
-                "ip": ip,
-                "port": port,
-                "endpoint": endpoint,
-                "protocol": protocol,
-                "transfer": transfer,
-                "encoding": encoding,
-                "hostname": hostname
-            }
-            profile = actuator_config[name].get("profile")
-            for key in actuator_config[name]:
-                if key in ("id", "profile"):
-                    continue
-                value = None
-                try:
-                    value = actuator_config[name].getint(key)
-                except ValueError:
-                    pass
-                try:
-                    if value is None:
-                        value = actuator_config[name].getboolean(key)
-                except ValueError:
-                    pass
-                try:
-                    if value is None:
-                        value = actuator_config[name].get(key)
-                except ValueError:
-                    pass
-                if value == "None":
-                    value = None
-                parameters[key] = value
-            actuators[(profile, name)] = clazz(**parameters)
+        ip = config["ip"]
+        port = config["port"]
+        endpoint = config["endpoint"]
+        transfer = config["transfer"]
+        encoding = config["encoding"]
+        configs = config["configs"]
 
-    # Load the encoder.
-    if encoding not in Encoders.__members__:
-        raise RuntimeError(f"{encoding} is not a registered encoding schema")
-    encoder = Encoders[encoding]
+        actuators = {}
+        for file in glob(f"{configs}/**/*.yaml", recursive=True):
+            with open(file) as f:
+                data = safe_load(f)
+                for name, values in data.items():
+                    print(f"Loading {name}...")
+                    identifier = values["id"]
+                    if identifier not in Actuators:
+                        raise RuntimeError(f"{identifier} is not a registered actuator")
+                    clazz = Actuators[identifier]
+                    parameters = dict(values)
+                    del parameters["id"]
+                    del parameters["profile"]
 
-    # Load the transferer (beautiful name, eh?).
-    if transfer not in Transfers:
-        raise RuntimeError(f"{transfer} is not a registered transfer schema")
-    transferer = Transfers[transfer](ip, port, endpoint)
+                    profile = values["profile"]
+                    actuators[(profile, name)] = clazz(**parameters)
 
-    consumer = Consumer("connector", actuators, encoder, transferer)
-    consumer.run()
+        # Load the encoder.
+        if encoding not in Encoders.__members__:
+            raise RuntimeError(f"{encoding} is not a registered encoding schema")
+        encoder = Encoders[encoding]
+
+        # Load the transferer (beautiful name, eh?).
+        if transfer not in Transfers:
+            raise RuntimeError(f"{transfer} is not a registered transfer schema")
+        transferer = Transfers[transfer](ip, port, endpoint)
+
+        consumer = Consumer("connector", actuators, encoder, transferer)
+        consumer.run()
 
 
 if __name__ == "__main__":
