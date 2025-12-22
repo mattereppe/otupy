4596ba36a8e48bd23fa18676c0695ab9601adfc0
Matteo Repetto <matteo.repetto@cnit.it>
2025-12-22 23:47:42 +0100

    Updated ctxd:network_type doc to warn about wrong implementation

--- src/otupy/profiles/ctxd/data/network_type.py
+++ src/otupy/profiles/ctxd/data/network_type.py
@@ -3,6 +3,14 @@ from otupy.core.register import Register
 
 #ATTENTION!! THIS IS NOT THE DEFINITION OF THE CTXD SPECIFICATION 
 class NetworkType(Choice):
+	""" Network type
+
+		The network type carries different configuration parameters, depending on the specific network 
+		technology.
+
+		WARNING: This definition is currently wrong, since it does not include network paramters.
+		It returns something like: "ethernet": "ethernet".
+	"""
 
 	register = Register({'ethernet': str, '802.11': str, '802.15': str, 'zigbee': str, 'vlan': str, 'vpn': str, 'lorawan': str, 'wan': str})
 
