11e75e8861df59b6acc85bbde525e16e8b86d9c3
Fabio Lorenzato <fabio.lorenzato@outlook.com>
2026-01-19 17:56:34 +0100

    feat: removed legacy context informations

--- src/otupy/profiles/xbom/targets/context.py
+++ src/otupy/profiles/xbom/targets/context.py
@@ -8,25 +8,17 @@ from otupy.core.target import target
 @oc2.target(name='context', nsid=Profile.nsid)
 class Context(oc2.types.base.Record):
 	""" Context
-		
-    	It describes the service environment, its connections and security capabilities.
+
+		It describes the service environment, its connections and security capabilities.
 	"""
-	services: ArrayOf(Name) = None # type: ignore
-	""" List the service names that the command refers to """
-	links: ArrayOf(Name) = None # type: ignore
-	""" List the link names that the command refers to """
+	boms: ArrayOf(Name) = None  # type: ignore
+	""" List the bom names that the command refers to """
 
-
-
-	def __init__(self, services = None, links = None):
-		self.services = ArrayOf(Name)(services) if services is not None else None
-		self.links = ArrayOf(Name)(links) if links is not None else None
-
+	def __init__(self, boms: list[Name] | None = None):
+		self.boms = ArrayOf(Name)(boms) if boms is not None else None
 
 	def __repr__(self):
-		return (f"Context(services={self.services}, links={self.links})")
-	
+		return f"Context(services={self.services}, links={self.links})"
+
 	def __str__(self):
-		return f"Context(" \
-	            f"services={self.services}, " \
-	            f"links={self.links})"
+		return f"Context(services={self.services}, links={self.links})"
