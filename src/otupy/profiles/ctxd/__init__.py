0df72b91d574cadd1f379b9c9c7aa83f28b8605e
Matteo Repetto <matteo.repetto@cnit.it>
2026-03-02 16:23:05 +0100

    Added vxlan support to ctxd host actuator

--- src/otupy/profiles/ctxd/__init__.py
+++ src/otupy/profiles/ctxd/__init__.py
@@ -21,15 +21,23 @@ from otupy.profiles.ctxd.results import Results
 from otupy.profiles.ctxd.validation import AllowedCommandTarget, AllowedCommandArguments, validate_command, validate_args
 
 # Make internal definitions available to external code
+from otupy.profiles.ctxd.data.service import Service, SId
+from otupy.profiles.ctxd.data.service_type import ServiceType
+from otupy.profiles.ctxd.data.link import Link
+from otupy.profiles.ctxd.data.link_type import LinkType
 from otupy.profiles.ctxd.data.cloud import Cloud
 from otupy.profiles.ctxd.data.name import Name
 from otupy.profiles.ctxd.data.consumer import Consumer
-from otupy.profiles.ctxd.data.container import Container
-from otupy.profiles.ctxd.data.pod import Pod
 from otupy.profiles.ctxd.data.port import Port, IPAddress, IPInfo
 from otupy.profiles.ctxd.data.execution_environment import ExecutionEnvironment
-from otupy.profiles.ctxd.data.link import Link
-from otupy.profiles.ctxd.data.link_type import LinkType
+from otupy.profiles.ctxd.data.execution_environment_type import ExecutionEnvironmentType
+from otupy.profiles.ctxd.data.os import OS
+from otupy.profiles.ctxd.data.container import Container
+from otupy.profiles.ctxd.data.host import Host
+from otupy.profiles.ctxd.data.host_type import HostType
+from otupy.profiles.ctxd.data.vm import VM
+from otupy.profiles.ctxd.data.pod import Pod
+from otupy.profiles.ctxd.data.iot import IoT
 from otupy.profiles.ctxd.data.network import Network
 from otupy.profiles.ctxd.data.network_node import NetworkNode
 from otupy.profiles.ctxd.data.network_interface import NetworkInterface
@@ -40,15 +48,13 @@ from otupy.profiles.ctxd.data.ethernet_network import EthernetNetwork
 from otupy.profiles.ctxd.data.ip_network import IPNetwork
 from otupy.profiles.ctxd.data.veth_network import VEthNetwork
 from otupy.profiles.ctxd.data.tunnel_network import TunnelNetwork
+from otupy.profiles.ctxd.data.vlan_network import VLANNetwork
+from otupy.profiles.ctxd.data.vxlan_network import VXLANNetwork
 from otupy.profiles.ctxd.data.network_type import NetworkType
 from otupy.profiles.ctxd.data.network_function import NetworkFunction
 from otupy.profiles.ctxd.data.network_function_type import NetworkFunctionType
-from otupy.profiles.ctxd.data.os import OS
 from otupy.profiles.ctxd.data.peer import Peer
 from otupy.profiles.ctxd.data.peer_role import PeerRole
-from otupy.profiles.ctxd.data.service_type import ServiceType
-from otupy.profiles.ctxd.data.service import Service
-from otupy.profiles.ctxd.data.vm import VM
 from otupy.profiles.ctxd.data.vm import HyperVisorType
 from otupy.profiles.ctxd.data.endpoint import Endpoint
 from otupy.profiles.ctxd.data.api import API
--- src/otupy/profiles/ctxd/data/os.py
+++ src/otupy/profiles/ctxd/data/os.py
@@ -8,28 +8,32 @@ class OS(Record):
 		and applications, as well as many subsystems (file systems, etc.).
 
 	"""
-	version: str = None
-	""" Version of the OS """
 	family: str = None
 	""" Family of the OS """
+	version: str = None
+	""" Version of the OS """
+	release: str = None
+	""" Release number/string """
 	arch: str = None
 	""" Supported CPU architecture """
 
 
-	def __init__(self, os = None, version = None, family = None, arch = None):
+	def __init__(self, os = None, version = None, family = None, release=None,  arch = None):
 		if os is not None:
-			self.version=os.version
 			self.family=os.family
+			self.version=os.version
+			self.release=os.release
 			self.arch=os.family
 		else:
-			self.version = str(version) 
 			self.family = str(family) 
+			self.version = str(version) 
+			self.release = str(release) 
 			self.arch = str(arch)
 
 
 	def __repr__(self):
 		return (f"OS("
-	             f"version={self.version}, family={self.family}, type={self.arch})")
+	             f"family={self.family}, version={self.version}, release={self.release}, type={self.arch})")
 	
 	def __str__(self):
 		return self.__repr__()
