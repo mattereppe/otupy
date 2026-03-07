fdb430469389159acda4e699681714239f45bdff
Matteo Repetto <matteo.repetto@cnit.it>
2026-03-07 23:44:06 +0100

    Revised 5G service model

--- src/otupy/profiles/ctxd/data/network.py
+++ src/otupy/profiles/ctxd/data/network.py
@@ -12,15 +12,19 @@ class Network(CTXDObject):
 	"""
 	type: NetworkType = None
 	""" Type of network (Ethernet, veth link, VLAN, ...) """
+	version: str = None
+	""" Version of the network implementation"""
 
 
-	def __init__(self, network = None, description = None, name = None, id = None, type = None):
+	def __init__(self, network = None, description = None, name = None, id = None, type = None, version=None):
 		if isinstance(network, Network):
 			super().__init__(name=network.name, description=network.description, id=network.id)
 			self.type = network.type
+			self.version=network.version
 		else:
 			super().__init__(name=name, description=description, id=id)
 			self.type = type 
+			self.version=version
 
 
 	def get_subtype(self):
@@ -28,7 +32,7 @@ class Network(CTXDObject):
 
 	def __repr__(self):
 		return (f"Network({super().__repr__()},"
-	             f"type={self.type.getObj()})")
+	             f"type={self.type.getObj()}, version={self.type.getObj()})")
 	
 	def __str__(self):
 		return self.__repr__()
