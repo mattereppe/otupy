0a754f15b79eeb65f5a7f5e7e3635b483a7db45c
Matteo Repetto <matteo.repetto@cnit.it>
2026-03-14 23:56:42 +0100

    Changed VEth peer data type

--- src/otupy/profiles/ctxd/data/veth_network.py
+++ src/otupy/profiles/ctxd/data/veth_network.py
@@ -6,13 +6,23 @@
 from otupy import Map, ArrayOf , Array
 from otupy.profiles.ctxd.data.ip_net_address import IPNetAddress
 
+class VEthPeer(Array):
+	""" VEth network interface peers
+
+		A tuple would be more appropriate to store a pair of information
+		(interface idx, namespace), but there is not such base type
+		in the OpenC2 language. An Array if the most fitting data type.
+	"""
+	pass
+
+
 class VEthNetwork(Map):
 	""" Virtual Ethernet link
 
 		A flexible container for network characteristics. 
 		Currently expects the network address/prefix as mandatory field.
 	"""
-	fieldtypes = dict(peers= ArrayOf(Array), nets = ArrayOf(IPNetAddress))
+	fieldtypes = dict(peers= ArrayOf(VEthPeer), nets = ArrayOf(IPNetAddress))
 	""" Field types
 	
 		This is the definition of the fields beard by the `Mobile` network.
