03efb22aa6dcb1a8d5ddc26403821e1e4145987c
Matteo Repetto <matteo.repetto@cnit.it>
2026-04-08 17:43:44 +0200

    Improved example producer for nfm to send different commands

--- examples/nfm_producer.py
+++ examples/nfm_producer.py
@@ -1,6 +1,14 @@
-"""This is a simple producer that can be used to test any NFM actuator."""
+""" This is a simple producer that can be used to test any NFM actuator.
+
+	Usage: <nfm_producer> [--start | --stop <id> | --query ]
+
+	--start returns the id of the nfm process. Take note of it and use it to stop the process later.
+
+"""
 
 import logging
+
+from argparse import ArgumentParser
 
 from otupy import Producer, Command, Actions, Features, Feature, Args, ResponseType, StatusCode, File
 from otupy.types.data.uri import URI
@@ -22,69 +30,88 @@ from otupy.actuators.nfm.nfm_actuator import NFMActuator  # Necessary for extend
 
 import otupy.profiles.nfm as nfm
 
+arguments = ArgumentParser()
+arguments.add_argument("--start", action="store_true", help="Start monitoring")
+arguments.add_argument("--stop", help="Stop monitoring the specified process")
+arguments.add_argument("--query", action="store_true", help="Stop monitoring the specified process")
+arguments.add_argument("--probe", default="fprobe", help="Select probe to run [fprobe|packetbeat]")
+args = arguments.parse_args()
 
 producer = Producer("nfm.example.net", JSONEncoder(), HTTPTransfer("127.0.0.1", 8080))
-actuator = Specifiers({"asset_id": "nfm-fprobe-example"})
+actuator = Specifiers({"asset_id": "nfm-"+args.probe})
 # actuator = Specifiers({"asset_id": "nfm-packetbeat-example"})
 arg = Args({"response_requested": ResponseType.complete})
 
 # First, we do the query action.
-cmd = Command(
-    Actions.query,
-    Features(
-        [
-            Feature.versions,
-            Feature.profiles,
-            Feature.pairs,
-            Feature.exports,
-            Feature.export_options,
-            Feature.flow_format,
-            Feature.filters,
-        ]
-    ),
-    arg,
-    actuator=actuator,
-)
-resp = producer.sendcmd(cmd)
-assert resp.status == StatusCode.OK
+if args.query:
+    print("Querying features...")
+    cmd = Command(
+        Actions.query,
+        Features(
+            [
+                Feature.versions,
+                Feature.profiles,
+                Feature.pairs,
+                Feature.exports,
+                Feature.export_options,
+                Feature.flow_format,
+                Feature.filters,
+            ]
+        ),
+        arg,
+        actuator=actuator,
+    )
+
 
 # The we try to start a flow monitor.
-collectors = ArrayOf(Collector)()
-collectors.append(Collector(address=IPv4Net("127.0.0.1"), port=Port(2055)))
-# For fprobe.
-arg = nfm.Args(
-    {
-        "exporter": Exporter(collectors=collectors),
-        "exporter_options": ExportOptions(format=FlowFormat.netflow7),
-    }
-)
-# For packetbeat.
-# arg = nfm.Args(
-#    {
-#        "exporter": Exporter(storage=File(name="test", path="/packetbeat-test")),
-#        "exporter_options": ExportOptions(format=FlowFormat.json),
-#    }
-# )
-interfaces = ArrayOf(Interface)()
-interfaces.append(Interface(name="en0"))
-ies = ArrayOf(IE)()
-ies.append(IE("source ip"))
-ies.append(IE("destination ip"))
+if args.start:
+    print("Starting nfm actuator...")
+    collectors = ArrayOf(Collector)()
+    collectors.append(Collector(address=IPv4Net("127.0.0.1"), port=Port(2055)))
+    # For fprobe.
+    arg = nfm.Args(
+        {
+            "exporter": Exporter(collectors=collectors),
+            "exporter_options": ExportOptions(format=FlowFormat.netflow7),
+        }
+    )
+    # For packetbeat.
+    # arg = nfm.Args(
+    #    {
+    #        "exporter": Exporter(storage=File(name="test", path="/packetbeat-test")),
+    #        "exporter_options": ExportOptions(format=FlowFormat.json),
+    #    }
+    # )
+    interfaces = ArrayOf(Interface)()
+    interfaces.append(Interface(name="en0"))
+    ies = ArrayOf(IE)()
+    ies.append(IE("source ip"))
+    ies.append(IE("destination ip"))
+    
+    ipv4_connections = [
+        # IPv4Connection(src_addr="192.168.1.1", dst_addr="130.251.17.2"),
+    ]
+    command = nfm.FlowMonitor(
+        interfaces=interfaces, information_elements=ies, filter_v4=ArrayOf(IPv4Connection)(ipv4_connections)
+    )
+    
+    cmd = Command(Actions.start, command, arg, actuator=actuator)
 
-ipv4_connections = [
-    # IPv4Connection(src_addr="192.168.1.1", dst_addr="130.251.17.2"),
-]
-command = nfm.FlowMonitor(
-    interfaces=interfaces, information_elements=ies, filter_v4=ArrayOf(IPv4Connection)(ipv4_connections)
-)
 
-cmd = Command(Actions.start, command, arg, actuator=actuator)
+if args.stop:
+    identifier = args.stop
+    cmd = Command(
+        Actions.stop, MonitorID(identifier), Args({"response_requested": ResponseType.complete}), actuator=actuator
+    )
+
+print("Command: ", cmd)
 resp = producer.sendcmd(cmd)
-print(resp)
-identifier = resp.content["results"]["monitor_id"]
+print("Got: ", resp)
 
-cmd = Command(
-    Actions.stop, MonitorID(identifier), Args({"response_requested": ResponseType.complete}), actuator=actuator
-)
-resp = producer.sendcmd(cmd)
-print(resp)
+if args.query:
+	assert resp.status == StatusCode.OK
+
+if args.stop:
+    identifier = resp.content["results"]["monitor_id"]
+
+
