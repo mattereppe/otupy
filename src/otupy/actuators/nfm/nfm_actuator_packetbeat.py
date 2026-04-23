08190531e1af2f9e084fbb342afd42331bb043e1
Matteo Repetto <matteo.repetto@cnit.it>
2026-04-23 08:36:42 +0200

    Fixed

--- src/otupy/actuators/nfm/nfm_actuator_packetbeat.py
+++ src/otupy/actuators/nfm/nfm_actuator_packetbeat.py
@@ -103,7 +103,6 @@ class NFMActuatorPacketbeat(NFMActuator):
 
         output = self._get_output(args)
         collectors = self._get_collectors(args)
-        print("Chiara Eva Catalano lo piglia nell'ano")
         monitor_id = generate_unique_name()
         config_file_name = self._configure_packetbeat_yaml(
             interfaces, information_elements, bpf_filters, output, collectors, sampling, monitor_id
